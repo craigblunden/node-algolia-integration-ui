8191cb71352bb8add679a3e81a8e0868d7c7dabb
craigblunden <craig@neto.com.au>
2018-08-23 22:03:44 +1000

    added MiniDrawer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,98 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios'
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
 
-class App extends Component {
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import BookmarksIcon from '@material-ui/icons/Bookmarks';
 
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: 430,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing.unit * 7,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing.unit * 9,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+class App extends React.Component {
   state = {
+    open: false,
     data: [],
     netoWebsiteURL: '',
     netoAPIUsername: '',
@@ -14,8 +100,8 @@ class App extends Component {
     algoliaAppID: '',
     algoliaAPIKey: '',
     algoliaIndex: '',
-  }
-  
+  };
+
   fetchData() {
     // Make a request for a user with a given ID
     let that = this
@@ -52,11 +138,67 @@ class App extends Component {
     this.fetchData();
   }
 
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
+    const { classes, theme } = this.props;
+
     return (
-      <div className="App">
-        <CssBaseline />
-        <form onSubmit={(e) => this.handleSubmit(e)} >
+      <div className={classes.root}>
+      <CssBaseline />
+        <AppBar
+          position="absolute"
+          className={classNames(classes.appBar, this.state.open && classes.appBarShift)}
+        >
+          <Toolbar disableGutters={!this.state.open}>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={this.handleDrawerOpen}
+              className={classNames(classes.menuButton, this.state.open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" noWrap>
+              Mini variant drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
+          }}
+          open={this.state.open}
+        >
+          <div className={classes.toolbar}>
+            <IconButton onClick={this.handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <BookmarksIcon />
+              </ListItemIcon>
+              <ListItemText primary="Servers" />
+            </ListItem>
+          </List>
+          <Divider />
+
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+
+          <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
+          <form onSubmit={(e) => this.handleSubmit(e)} >
           <label>netoWebsiteURL</label>
           <input type="text" name="netoWebsiteURL" onChange={(e) => this.handleChange(e)} value={this.state.netoWebsiteURL}/>
           <br />
@@ -82,9 +224,17 @@ class App extends Component {
           this.state.data.map(itm => <li key={itm.objectID}>{itm.Name}</li>)
         }
         </ul>
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(App);
